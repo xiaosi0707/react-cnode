31e496153e90b824afd2be514bc15b13c5c9b549
wyunfei <583514368@qq.com>
2018-09-14 21:53:30 +0800

    loading组件引入

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CnodeRouter from './router/index'
 import CHeader from './components/common/header'
 import './assets/css/app.scss';
+import ReactLoading from 'react-loading'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
           <CHeader></CHeader>
           <CnodeRouter></CnodeRouter>
+          <ReactLoading type='balls' color='#80BD01' className='loading'></ReactLoading>
       </div>
     );
   }
